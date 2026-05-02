8f2583f171b4302d1bc9d3ae4b1965e389e05673
Jose Leiva <email.joseleiva@gmail.com>
2026-05-01 22:34:41 -0400

    Download dataset into a custom output dir

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 import kagglehub
 
-FOLLOWERS_CSV = "edges.csv"  # follower_id, followee_id
-POSTS_CSV = "user_tweets.csv"  # id, tweet_1, tweet_2, ..., tweet_
-USERS_CSV = "user_info.csv"  # id, name, description, followers_count, friends_count
+# columnas relevantes: follower_id, followee_id
+FOLLOWERS_CSV = "edges.csv"
+# columnas relevantes: id, tweet_1, tweet_2, ..., tweet_200
+POSTS_CSV = "user_tweets.csv"
+# columnas relevantes: id, name, description, followers_count, friends_count
+USERS_CSV = "user_info.csv"
 
-
-dataset_path = kagglehub.dataset_download("sanketrai/twitter-mbti-dataset")
+# C:\Users\tkdgi\.cache\kagglehub\datasets\sanketrai\twitter-mbti-dataset\versions\2
+dataset_path = kagglehub.dataset_download(
+    "sanketrai/twitter-mbti-dataset", output_dir="./data"
+)
 print("Path to dataset files:", dataset_path)
